45f0abc2f42e77187554a2bca961e24852039c18
Dijar Kadriu <ddijarr@gmail.com>
2020-01-15 16:38:14 +0100

    End of Section 9

--- DatingApp-SPA/src/app/members/member-list/member-list.component.spec.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.spec.ts
@@ -2,8 +2,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { MemberListComponent } from '../members/member-list/member-list.component';
 
+import { MemberListComponent } from './member-list.component';
 
 describe('MemberListComponent', () => {
   let component: MemberListComponent;
@@ -11,9 +11,9 @@ describe('MemberListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MemberListComponent]
+      declarations: [ MemberListComponent ]
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
